f7af0746e63831723726edbc0dbd445b65c035a5
Kevin Truong <42168530+kevinoli@users.noreply.github.com>
2024-07-31 02:15:41 -0600

    reformatting

--- run.sh
+++ run.sh
@@ -6,12 +6,13 @@ lsof -t -i:33333 | xargs -r kill -9
 lsof -t -i:52525 | xargs -r kill -9
 
 # Start the server processes
-cd server
+cd server/node
 node server.js &
 NODE_PID=$!
+cd ../flask
 python app.py &
 PYTHON_PID=$!
-cd ../client
+cd ../../client
 npm start
 
 # Kill processes on specified ports before starting new ones
